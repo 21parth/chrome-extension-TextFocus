51afdfa42006cbd03b068062c76fe735be8c6662
Parth Patel <parth05211@gmail.com>
2023-11-22 20:56:45 -0500

    Added the callback function which changes the text on the icon ON/OFF

--- background.js
+++ background.js
@@ -1,5 +1,18 @@
+//set the initial text on the extension icon (badge text)
 chrome.runtime.onInstalled.addListener(()=>{
     chrome.action.setBadgeText({
         text:"OFF",
     });
+});
+
+//Callback funtion is called when the extension icon is clicked.
+chrome.action.onClicked.addListener(async (tab) => {
+    if(true){
+        const prevState = await chrome.action.getBadgeText({tabId: tab.id}) //getting the badge text
+        const nextState = prevState === "ON" ? "OFF" : "ON" //set the next badge text
+        await chrome.action.setBadgeText({
+            tabId: tab.id,
+            text: nextState
+        })
+    }
 });
